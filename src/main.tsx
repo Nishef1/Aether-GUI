4c364497b27373fc8496ef595698e59dadb36806
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-07-27 17:09:10 -0400

    perf(android): expose platform for low-power visual rules

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
+import { isAndroid } from "./lib/platform";
+
+document.documentElement.dataset.platform = isAndroid ? "android" : "desktop";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
