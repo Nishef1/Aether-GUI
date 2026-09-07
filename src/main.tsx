f6f5e06e54065b2463cf9f944c30b9d4a6bd211a
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-09-08 01:20:30 +0330

    refactor(ui): keep motion policy at app boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MotionConfig } from "motion/react";
 
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MotionConfig reducedMotion="user">
-      <App />
-    </MotionConfig>
+    <App />
   </StrictMode>,
 );
